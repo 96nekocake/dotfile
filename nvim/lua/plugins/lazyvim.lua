6a64c5ef7c97e73f94b1e8eb143f1fa15e1221d7
96nekocake <96nekocake@gmail.com>
2024-11-13 16:07:09 +0900

    first commit

---
+++ nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      colorscheme = "iceberg",
+    },
+  },
+  { "folke/lazy.nvim", version = false },
+  { "LazyVim/LazyVim", version = false },
+}
